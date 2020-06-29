78d5e41b4d8e7855a60b777a47a8e589acbed6de
Ashleigh McGinnis <ashleighmcginnis@MacBook-Pro.local>
2020-06-29 14:13:05 -0500

    update box to turn red

---
+++ app.js
@@ -0,0 +1,26 @@
+$(document).ready(onReady);
+
+let clickcount = 0;
+
+function onReady(){
+    $('.button_generate').on('click', Box);
+    $('.button_yellow').on('click', makeBoxYellow)
+
+}
+
+function Box(){
+    clickcount ++;
+    $('.js_container').append(`
+        <div class="Box">
+    <p>${clickcount}</p>
+    </div>
+    `)
+}
+
+function makeBoxYellow(){
+    clickcount++
+    $('.js_container').append(`
+        <div class "Box yellow"
+    <p>${clickcount}</p>
+    </div>`)
+}
